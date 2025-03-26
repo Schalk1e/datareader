883540e9bc4534aa767f901c3d9463e34d2ded34
Schalk <sv973005@gmail.com>
2025-03-26 22:38:24 +0200

    Slight reordering.

--- src/datareader/parser/sql_parser.py
+++ src/datareader/parser/sql_parser.py
@@ -10,46 +10,6 @@ from datareader.helpers import _build_df_dict
 from .parser_abc import Parser
 
 
-def _bracket_extract(input_string: str) -> str:
-    """Extracts text between (outermost) brackets rounded brackets.
-
-    Args:
-        input_string: The string to search over.
-
-    Returns:
-        The text within the outermost brackets.
-    """
-    match_object = re.search(r"\((.*)\)", input_string)
-    if match_object:
-        return match_object.group(1)
-    return ""
-
-
-def _list_split(input_list: list[Any], n: int) -> list[Any]:
-    """Splits an input list into individual lists of size n and remainder.
-
-    Args:
-        input_string: The string to search over.
-        n: Split size.
-
-    Returns:
-        A list of split lists.
-    """
-    return [input_list[i : i + n] for i in range(0, len(input_list), n)]
-
-
-def _get_first_words(input_list: list[str]) -> list[str]:
-    """Finds the first words delimited with a space in a list of strings.
-
-    Args:
-        input_list: List of strings.
-
-    Returns:
-        A list of strings containing the first words in each input string.
-    """
-    return [x.strip().split(" ")[0] for x in input_list]
-
-
 @define
 class SQLParser(Parser):
     path: Path
@@ -89,3 +49,43 @@ class SQLParser(Parser):
         # First list element is the header list.
         # _build_df_dict takes data and headers separately.
         return DataFrame(_build_df_dict(tbl[1:], headers=headers))
+
+
+def _bracket_extract(input_string: str) -> str:
+    """Extracts text between (outermost) brackets rounded brackets.
+
+    Args:
+        input_string: The string to search over.
+
+    Returns:
+        The text within the outermost brackets.
+    """
+    match_object = re.search(r"\((.*)\)", input_string)
+    if match_object:
+        return match_object.group(1)
+    return ""
+
+
+def _list_split(input_list: list[Any], n: int) -> list[Any]:
+    """Splits an input list into individual lists of size n and remainder.
+
+    Args:
+        input_string: The string to search over.
+        n: Split size.
+
+    Returns:
+        A list of split lists.
+    """
+    return [input_list[i : i + n] for i in range(0, len(input_list), n)]
+
+
+def _get_first_words(input_list: list[str]) -> list[str]:
+    """Finds the first words delimited with a space in a list of strings.
+
+    Args:
+        input_list: List of strings.
+
+    Returns:
+        A list of strings containing the first words in each input string.
+    """
+    return [x.strip().split(" ")[0] for x in input_list]
